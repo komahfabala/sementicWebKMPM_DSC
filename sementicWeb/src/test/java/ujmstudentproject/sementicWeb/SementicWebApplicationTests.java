1d31d328ee87ed9fbcfb818b23e79f3c19aae051
komahfabala <komahfabala@gmail.com>
2021-12-10 15:07:25 +0100

    initilisation

---
+++ sementicWeb/src/test/java/ujmstudentproject/sementicWeb/SementicWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ujmstudentproject.sementicWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SementicWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
