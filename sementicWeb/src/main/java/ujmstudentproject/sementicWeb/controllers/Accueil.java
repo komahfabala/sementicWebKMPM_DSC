c54f3877405b137976520b5b5b0991dfae5744e9
komahfabala <komahfabala@gmail.com>
2021-12-22 22:18:54 +0100

    initialisation projet

--- sementicWeb/src/main/java/ujmstudentproject/sementicWeb/controllers/Accueil.java
+++ sementicWeb/src/main/java/ujmstudentproject/sementicWeb/controllers/Accueil.java
@@ -3,7 +3,7 @@ package ujmstudentproject.sementicweb.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 
 import ujmstudentproject.sementicweb.services.*;
 
