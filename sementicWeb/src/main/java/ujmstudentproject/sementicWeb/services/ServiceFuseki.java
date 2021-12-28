18179d80336d998e441b08e821279206c9ab7cd8
PamPierre <pampierre20@gmail.com>
2021-12-29 00:40:43 +0100

    triple stor of building

--- sementicWeb/src/main/java/ujmstudentproject/sementicWeb/services/ServiceFuseki.java
+++ sementicWeb/src/main/java/ujmstudentproject/sementicWeb/services/ServiceFuseki.java
@@ -5,7 +5,8 @@ import java.util.*;
 import org.apache.jena.rdf.model.Model;
 
 public interface ServiceFuseki{
-    public void readCSV(String filePath);
+    public void readCSV_From_Meteo(String filePath);
+    public void readCSV_From_Territoire(String filePath);
     public void loadTTL(String filepath);
     public Model weather(Map<String, ArrayList<String>> tempValueMap, String code);
     public void createCon();
